ca6c86bc4470ef172d0ef599376f1a4c4202da0e
vitiv <ivvisetg@gmail.com>
2025-02-26 23:15:43 +0200

    правки по css, таблица с эффектом выдавливания

--- consultation.php
+++ consultation.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+if (!isset($_SESSION["user_id"])) {
+    header("Location: login.php");
+    exit(); // Останавливаем выполнение кода после редиректа
+}
 ?>
 <!DOCTYPE html>
 <html lang="ru">
